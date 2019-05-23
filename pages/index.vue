7362af84d68fec5cf455d680293a91603ad6ee68
Faisal amrullah <amrulahfaisal@gmail.com>
2019-05-24 03:59:40 +0700

    add child component

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,7 @@
   <section class="wrapper">
     <div class="home-page">
       <div class="home-page__layout">
-        <li v-for="(item, index) in data1" :key="item[index]" class="timeline">
-          <div
-            class="border"
-            :class="{
-              hidden: index === data1.length - 1,
-              isSentBorder: item.status === 'sent',
-              isApprovedBorder: item.status === 'approved'
-            }"
-          ></div>
-          <div class="img-container">
-            <img
-              :src="item.photo"
-              class="img"
-              :class="{
-                isWaitingBorderRadius: item.status === '',
-                isSentBorderRadius: item.status === 'sent',
-                isApprovedBorderRadius: item.status === 'approved'
-              }"
-            />
-          </div>
-          <div class="text ">
-            <p class="name">{{ item.first_name }} {{ item.last_name }}</p>
-            <p
-              class="message"
-              :class="{
-                isSentText: item.status === 'sent'
-              }"
-            >
-              {{ item.message }}
-            </p>
-            <p class="date">{{ item.date_time }}</p>
-          </div>
-          <!-- <button @click="testFunc(data1.length)">klik</button> -->
-        </li>
+        <leftComponent :data="data1" />
       </div>
       <div class="home-page__layout">
         <div v-for="(item, index) in data2" :key="item[index]">
@@ -49,20 +16,18 @@
 <script>
 import './index.scss'
 import { data1, data2 } from '../data'
+import leftComponent from './left-component'
 
 export default {
-  components: {},
+  components: {
+    leftComponent
+  },
   data() {
     return {
       data1: data1,
-      data2: data2,
-      show: data1.length
+      data2: data2
     }
   },
-  methods: {
-    testFunc(index) {
-      console.log(index)
-    }
-  }
+  methods: {}
 }
 </script>
---
+++ pages/left-component.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <li v-for="(item, index) in data" :key="item[index]" class="timeline">
+      <div
+        class="border"
+        :class="{
+          hidden: index === data.length - 1,
+          isSentBorder: item.status === 'sent',
+          isApprovedBorder: item.status === 'approved'
+        }"
+      ></div>
+      <img
+        :src="item.photo"
+        class="img"
+        :class="{
+          isWaitingBorderRadius: item.status === '',
+          isSentBorderRadius: item.status === 'sent',
+          isApprovedBorderRadius: item.status === 'approved'
+        }"
+      />
+      <div class="text ">
+        <p class="name">{{ item.first_name }} {{ item.last_name }}</p>
+        <p
+          class="message"
+          :class="{
+            isSentText: item.status === 'sent',
+            isApprovedText: item.status === 'approved'
+          }"
+        >
+          {{ item.message }}
+        </p>
+        <p v-if="item.status === 'approved'" class="note">{{ item.note }}</p>
+        <p class="date">
+          {{ item.date_time }}
+        </p>
+        <p v-if="item.status === ''">-</p>
+      </div>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  props: ['data']
+}
+</script>
